ca1bd302a8392c7b8a78d90f4b5d418b1bbeeffc
Traphume <traphumedev@gmail.com>
2020-02-20 18:27:50 +0700

    [CheckStatus] Include esapi.Response as return value [AddPet] Added AddPet interface and concrete type [PetModel] Added schema of document for Pet resource

--- search/pet.go
+++ search/pet.go
@@ -1,13 +1,24 @@
 package search
 
 import (
+	"bytes"
+	"context"
+	"encoding/json"
 	"errors"
 	"fmt"
 	"github.com/elastic/go-elasticsearch/v7"
+	"github.com/elastic/go-elasticsearch/v7/esapi"
+	"github.com/google/uuid"
 )
 
+type PetModel struct {
+	Name string `json:"name"`
+	Desc string `json:"desc"`
+}
+
 type Pet interface {
-	CheckStatus() error // Must return nil if there is no error
+	CheckStatus() (*esapi.Response, error)
+	AddPet(context.Context, PetModel) (*esapi.Response, error)
 }
 
 // Concrete implementation
@@ -15,11 +26,33 @@ type PetClient struct {
 	es *elasticsearch.Client
 }
 
-func (pc *PetClient) CheckStatus() error {
-	_, err := pc.es.Info()
+func (pc *PetClient) CheckStatus() (*esapi.Response, error) {
+	res, err := pc.es.Info()
 	if err != nil {
-		return errors.New(fmt.Sprintf("cluster returned error: %s", err))
+		return nil, errors.New(fmt.Sprintf("cluster returned error: %s", err))
 	}
 
-	return nil
+	return res, nil
+}
+
+func (pc *PetClient) AddPet(ctx context.Context, pm PetModel) (*esapi.Response, error) {
+	id := uuid.New()
+	bodyBytes, err := json.Marshal(pm)
+	if err != nil {
+		return nil, errors.New(fmt.Sprintf("could not marshal struct: %s", err))
+	}
+
+	req := esapi.IndexRequest{
+		Index:      id.String(),
+		DocumentID: pm.Name,
+		Body:       bytes.NewReader(bodyBytes),
+		Refresh:    "true",
+	}
+
+	res, err := req.Do(ctx, pc.es)
+	if err != nil {
+		return nil, errors.New(fmt.Sprintf("could not index document: %s", err))
+	}
+
+	return res, nil
 }
