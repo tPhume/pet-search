eaf6364b340becbb5a8402fefb40952681947d49
Traphume <traphumedev@gmail.com>
2020-02-25 03:03:04 +0700

    Finish the validation in constructor of [PetInstance]

--- model/pet_test.go
+++ model/pet_test.go
@@ -8,18 +8,35 @@ func TestNewPetInstance(t *testing.T) {
 		t.Fatal(err)
 	}
 
-	_, err = NewPetInstance("", "")
-	if err != errorEmptyName {
-		t.Fatalf("expected [%s], got = [%s]", errorEmptyName, err)
-	}
-
 	_, err = NewPetInstance("h", "")
 	if err != errorShortName {
 		t.Fatalf("expected [%s], got = [%s]", errorShortName, err)
 	}
 
-	_, err = NewPetInstance("12345678901234567890123456789012", "")
+	_, err = NewPetInstance("123456789012345678901234567890123", "")
 	if err != errorLongName {
 		t.Fatalf("expected [%s], got = [%s]", errorLongName, err)
+	}
+}
+
+func TestNewPetInstanceWithId(t *testing.T) {
+	_, err := NewPetInstanceWithId("1", "sushi", "This is a description.")
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	_, err = NewPetInstanceWithId("", "", "")
+	if err != errorShortId {
+		t.Fatalf("expected [%s], got = [%s]", errorShortId, err)
+	}
+
+	tooLong := "0"
+	for i := 1; i < 129; i++ {
+		tooLong += string(i)
+	}
+
+	_, err = NewPetInstanceWithId(tooLong, "", "")
+	if err != errorLongId {
+		t.Fatalf("expected [%s], got = [%s]", errorLongId, err)
 	}
 }
