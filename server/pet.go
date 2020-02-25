da686f81b6b8e0cd9813f60e1950ad49a39fabc5
Traphume <traphumedev@gmail.com>
2020-02-25 23:35:11 +0700

    [addPetHandler] Added Added petRequest to package

--- server/pet.go
+++ server/pet.go
@@ -2,7 +2,9 @@ package server
 
 import (
 	"github.com/gin-gonic/gin"
+	"github.com/tPhume/pet-search/model"
 	"github.com/tPhume/pet-search/search"
+	"net/http"
 )
 
 // to marshal for response
@@ -16,8 +18,17 @@ type petResponseList struct {
 	Result []*petResponse `json:"result"`
 }
 
+// to bind to request body
+type petRequest struct {
+	Name string `json:"name"`
+	Desc string `json:"desc"`
+}
+
 func RegisterPetRoutes(router *gin.Engine, search search.Pet) {
 	router.Use(setSearch(search))
+
+	v1 := router.Group("/api/v1/pets")
+	v1.POST("", addPetHandler)
 }
 
 // returns handler with search passed into value
@@ -25,4 +36,34 @@ func setSearch(search search.Pet) gin.HandlerFunc {
 	return func(c *gin.Context) {
 		c.Set("search", search)
 	}
+}
+
+// handler to add pet
+func addPetHandler(ctx *gin.Context) {
+	temp, ok := ctx.Get("search")
+	if !ok {
+		ctx.JSON(http.StatusInternalServerError, nil)
+		return
+	}
+
+	s := temp.(search.Pet)
+	body := petRequest{}
+	if err := ctx.ShouldBindJSON(&body); err != nil {
+		ctx.JSON(http.StatusBadRequest, nil)
+		return
+	}
+
+	pm, err := model.NewPetInstance(body.Name, body.Desc)
+	if err != nil {
+		ctx.JSON(http.StatusBadRequest, nil)
+		return
+	}
+
+	id, err := s.AddPet(ctx, pm)
+	if err != nil {
+		ctx.JSON(http.StatusBadRequest, nil)
+		return
+	}
+
+	ctx.JSON(http.StatusCreated, gin.H{"id": id})
 }
