d78102df93e47635278a75546cd3d20c9727c46c
Traphume <traphumedev@gmail.com>
2020-03-20 01:29:55 +0700

    Added consumer for AddPet

---
+++ consumer/consumer.go
@@ -0,0 +1,81 @@
+package consumer
+
+import (
+	"bytes"
+	"context"
+	"encoding/gob"
+	"github.com/streadway/amqp"
+	"github.com/tPhume/pet-search/model"
+	"github.com/tPhume/pet-search/search"
+	"log"
+)
+
+// simple consumer that will consume and run AddPet
+func ConsumePet(search search.Pet) {
+	conn, err := amqp.Dial("amqp://guest:guest@localhost:9200")
+	failOnError("fail to connect to rabbitmq", err)
+
+	ch, err := conn.Channel()
+	failOnError("fail to create a channel", err)
+
+	err = ch.ExchangeDeclare(
+		"pet",
+		amqp.ExchangeDirect,
+		true,
+		false,
+		false,
+		false,
+		nil,
+	)
+	failOnError("could not declare exchange", err)
+
+	queue, err := ch.QueueDeclare(
+		"Add pet",
+		true,
+		false,
+		false,
+		false,
+		nil,
+	)
+	failOnError("could not declare queue", err)
+
+	err = ch.QueueBind(queue.Name, "pet.add", "pet", false, nil)
+	failOnError("fail to bind queue", err)
+
+	msgs, err := ch.Consume(
+		queue.Name,
+		"consume add pet",
+		false,
+		false,
+		false,
+		false,
+		nil,
+	)
+	failOnError("could not start consumer", err)
+
+	for d := range msgs {
+		err = nil
+
+		body := model.PetInstance{}
+
+		dec := gob.NewDecoder(bytes.NewReader(d.Body))
+		err = dec.Decode(&body)
+
+		if err != nil {
+			_ = d.Nack(false, true)
+		} else {
+			_, err = search.AddPet(context.Background(), &body)
+			if err != nil {
+				_ = d.Nack(false, true)
+			}
+		}
+
+		_ = d.Ack(false)
+	}
+}
+
+func failOnError(msg string, err error) {
+	if err != nil {
+		log.Fatalf("%s: %s", msg, err)
+	}
+}
