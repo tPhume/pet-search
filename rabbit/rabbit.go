87301c9bb4f181cdae3bbd71ce0df7e1bfee5858
Traphume <traphumedev@gmail.com>
2020-03-20 00:45:16 +0700

    Added concrete implementation of PetClient that will work with RabbitMQ

--- rabbit/rabbit.go
+++ rabbit/rabbit.go
@@ -1,7 +1,10 @@
 package rabbit
 
 import (
+	"bytes"
 	"context"
+	"encoding/gob"
+	"github.com/streadway/amqp"
 	"github.com/tPhume/pet-search/model"
 )
 
@@ -9,5 +12,57 @@ import (
 type Pet interface {
 	AddPet(context.Context, model.PetModel) error
 	UpdatePet(context.Context, model.PetModel) error
-	DeletePet(context.Context, string) error
+}
+
+// Concrete implementation
+type PetClient struct {
+	channel *amqp.Channel
+}
+
+func (pc *PetClient) AddPet(ctx context.Context, petModel model.PetModel) error {
+	var body bytes.Buffer
+	enc := gob.NewEncoder(&body)
+
+	if err := enc.Encode(petModel); err != nil {
+		return err
+	}
+
+	if err := pc.channel.Publish(
+		"pet",
+		"pet.add",
+		false,
+		false,
+		amqp.Publishing{
+			Type: "gob",
+			Body: body.Bytes(),
+		},
+	); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (pc *PetClient) UpdatePet(ctx context.Context, petModel model.PetModel) error {
+	var body bytes.Buffer
+	enc := gob.NewEncoder(&body)
+
+	if err := enc.Encode(petModel); err != nil {
+		return err
+	}
+
+	if err := pc.channel.Publish(
+		"pet",
+		"pet.update",
+		false,
+		false,
+		amqp.Publishing{
+			Type: "gob",
+			Body: body.Bytes(),
+		},
+	); err != nil {
+		return err
+	}
+
+	return nil
 }
